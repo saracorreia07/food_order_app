08ccc9fb1a350f34da56bbbd903eb8b3c8225b87
Sara Correia <sara.dsc@hotmail.com>
2021-12-14 16:31:19 +0000

    fixed input ids

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -34,6 +34,7 @@ const AvailableMeals = () => {
   //transform the js array into a jsx array
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
+      id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}
