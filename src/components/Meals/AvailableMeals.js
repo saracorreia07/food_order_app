86e35d7fa3440b84a0b0cc48f532e1cc13c675c2
Sara Correia <sara.dsc@hotmail.com>
2021-12-14 14:36:53 +0000

    display of meal items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 //dummy data
@@ -30,11 +32,20 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   //transform the js array into a jsx array
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import classes from "./Card.module.css";
+
+//wrapper component with HTML/CSS code
+const Card = (props) => {
+  return <div className={classes.card}>{props.children}</div>;
+  //whatever is passed in the Card component is used inside of Card through {props.children}
+};
+
+export default Card;
