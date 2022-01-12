65407e297a6e5a84f7172b18ff6445025e24c6a6
Sara Correia <sara.dsc@hotmail.com>
2022-01-12 12:10:06 +0000

    finished submission form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
+import { useContext } from "react";
+
 import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`; // always renders price with 2 decimals
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   //props to render meal items in the DUMMY_MEALS array
   return (
@@ -13,7 +26,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,15 @@
+import React from "react";
+
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />{" "}
+      <input ref={ref} {...props.input} />
       {/*spread operator ensures that all key-value pairs in this input object are added as props to input*/}
     </div>
   );
-};
+});
 
 export default Input;
